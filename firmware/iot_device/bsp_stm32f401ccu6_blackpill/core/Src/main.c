a501412ff380283d1e2b55e7a9732ba2ae24a585
Pavel Tribunsky <tribunskiy_p@stilsoft.ru>
2024-11-13 00:01:43 +0300

    FreeRTOS files added to project

--- firmware/iot_device/bsp_stm32f401ccu6_blackpill/core/Src/main.c
+++ firmware/iot_device/bsp_stm32f401ccu6_blackpill/core/Src/main.c
@@ -27,6 +27,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "app_general.h"
+#include "FreeRTOS.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
